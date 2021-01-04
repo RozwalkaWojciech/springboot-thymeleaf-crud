0e1c355a4195dae1b3d0b89c0fa534627e334d60
Wojciech <wojciechrozwalka@gmail.com>
2021-01-04 22:50:32 +0100

    add StudentRepository interface

---
+++ src/main/java/javer/springbootthymeleafcrud/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package javer.springbootthymeleafcrud.repository;
+
+import javer.springbootthymeleafcrud.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    List<Student> findByName(String name);
+}
