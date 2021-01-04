4983ffb0d4f6a46a3294c3cc8b12929eb45697a3
Wojciech <wojciechrozwalka@gmail.com>
2021-01-04 23:54:28 +0100

    change to 2.1.4.RELEASE spring boot version, change test, everything is work

--- src/test/java/javer/springbootthymeleafcrud/SpringbootThymeleafCrudApplicationTests.java
+++ src/test/java/javer/springbootthymeleafcrud/SpringbootThymeleafCrudApplicationTests.java
@@ -1,13 +1,16 @@
 package javer.springbootthymeleafcrud;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootThymeleafCrudApplicationTests {
+public class SpringbootThymeleafCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
