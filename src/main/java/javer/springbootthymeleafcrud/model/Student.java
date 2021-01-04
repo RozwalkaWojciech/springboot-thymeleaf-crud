dab1574414e46bf790d045e6f67d92e0ff20837e
Wojciech <wojciechrozwalka@gmail.com>
2021-01-04 22:32:47 +0100

    add entity student with lombok

---
+++ src/main/java/javer/springbootthymeleafcrud/model/Student.java
@@ -0,0 +1,29 @@
+package javer.springbootthymeleafcrud.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    private long phoneNumber;
+}
