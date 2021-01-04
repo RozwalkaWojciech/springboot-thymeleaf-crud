2088424daa5a2e663a8bbcd4497c8581da904f78
Wojciech <wojciechrozwalka@gmail.com>
2021-01-04 22:20:24 +0100

    init commit

---
+++ src/test/java/javer/springbootthymeleafcrud/SpringbootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package javer.springbootthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
